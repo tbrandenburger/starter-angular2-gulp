0a3cf96e93c6eb30fc53c6b2c441a686213a07d3
Thomas Brandenburger <mail@thomasbrandenburger.ch>
2016-08-31 21:19:08 +0200

    firstcommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2223 App</h1>'
+})
+export class AppComponent implements OnInit{
+
+    ngOnInit():void {
+        console.log("ölkölkölk");
+    }
+
+}
